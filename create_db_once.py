ebb8f2ffd39a651f213f680bd3472fb57fea5279
Sergei Kruchinin <siblis@ya.ru>
2024-09-23 11:35:04 +0300

    create_db_once fix, deleted useless flask app context

--- create_db_once.py
+++ create_db_once.py
@@ -1,20 +1,17 @@
 # create_db_once.py
 
-from flask_app import create_app
 from sqlalchemy.orm import sessionmaker, Session
 from core.models.base import init_db, engine, Base
 from core.models.user import User
 from core.schemas import ManualUserCreateSchema
 
-app = create_app()
 
-SessionLocal = sessionmaker(bind=engine)
-
-with app.app_context():
+def create_db_once():
+    session_local = sessionmaker(bind=engine)
     Base.metadata.drop_all(bind=engine)  # for debug only
     init_db()
 
-    db: Session = SessionLocal()
+    db: Session = session_local()
 
     try:
         user_data = ManualUserCreateSchema(
@@ -38,3 +35,7 @@ with app.app_context():
         print(f"Error occurred: {e}")
     finally:
         db.close()
+
+
+if __name__ == "__main__":
+    create_db_once()
